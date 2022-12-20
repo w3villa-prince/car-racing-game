34a7bf973e1381984e05151b60cb94cc16b38efc
w3villa-prince <prince.yadav@w3villa.com>
2022-12-20 15:11:07 +0530

    Add Main Screen demo

--- Assets/Scripts/detectcollision.cs
+++ Assets/Scripts/detectcollision.cs
@@ -17,4 +17,23 @@ public class detectcollision : MonoBehaviour
         Destroy(gameObject);
         Destroy(other.gameObject);
     }
+
+    /* public float AreaOfTrapezium(float a, float b, float c, float d)
+     {
+         float h = c - a;
+         float area = (b + d) * h / 2;
+         return area;
+     }
+
+     public float AreaOfArray(float[,] array2d)
+     {
+         float areaofArray = 0;
+         int row = array2d.Length;
+         for (int i = 0; i < row - 1; i++)
+         {
+             Debug.Log(i);
+             areaofArray += AreaOfTrapezium(array2d[i, 0], array2d[i, 1], array2d[i + 1, 0], array2d[i + 1, 1]);
+         }
+         return areaofArray;
+     }*/
 }
